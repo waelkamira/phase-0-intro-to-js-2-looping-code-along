d131f0e9a4dd7d46ccda103791b71e28b0d5d1f2
wael kamira <ramondm4@gmail.com>
2023-04-11 01:23:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(name, surprise){
+    let myArr = [];
+    for(let i = 0; i < name.length; i++){
+        myArr.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return myArr
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+function countDown(num){
+    
+    while ( num >= 0 ){
+        console.log(num);
+        num--;
+    }
+    return num
+}
+
+countDown(10);
+
+
